d7962e44f85efb98a1ad1341d273334a2d50dc8d
Bhamarim <105685328+Bhamarim@users.noreply.github.com>
2023-04-26 18:33:01 -0300

    initial commit

--- scripts/bham-chat-cards-core.js
+++ scripts/bham-chat-cards-core.js
@@ -4,4 +4,15 @@ class CoreCardHandler {
   static messageFormula(message, html) {}
 }
 
-Hooks.on('renderChatMessage', (chatMessage, html) => {})
+Hooks.on('renderChatMessage', (chatMessage, html) => {
+  console.log(chatMessage.flags, 'bham chat message')
+  console.log(html, 'bham html')
+
+  if (chatMessage.flags.pf2e.context.type == 'damage-roll') {
+    console.log('bham foi')
+    console.log($(html).children(), 'bham children')
+
+    // $(html).children('message-content').children('damage-application').hide()
+    // $(html).hide()
+  }
+})
